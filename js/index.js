369256b2068b8ad6bd9137bc27671d0076c10eaa
Littlewhite <964873559@qq.com>
2016-03-03 13:29:31 +0800

    timeline

--- js/index.js
+++ js/index.js
@@ -1,12 +1,35 @@
-window.onload = function  () {
-	
-}
+
+//获取屏幕的宽度
+var windowH ;
+var start ;
+var end ;
+var time;
 
 //滚屏方法
 function  scrollDown(id) {
-	//获取屏幕的宽度
-	var windowH = document.body.clientHeight;
-	for (var i = 0; i < windowH*id; i++) {
-		parent.scroll(1,i);
+	//初始化
+	windowH = document.body.clientHeight;
+	start = 0;
+	end = 100;
+	if (id>0) {
+		windowH = windowH*id;
+		scrollBegin();
 	}
+}
+
+function scrollBegin () {
+	if (end>=windowH) {
+		end = windowH;
+		for (var i = start; i <= end; i++) {
+			parent.scroll(1,i);
+		};
+		clearTimeout(time);
+		return;
+	};
+	for (var i = start; i < end; i++) {
+			parent.scroll(1,i);
+	};
+	start = end;
+	end += 100;
+	time = setTimeout("scrollBegin()",50);
 }
